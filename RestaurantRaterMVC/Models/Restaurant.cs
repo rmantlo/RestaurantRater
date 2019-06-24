7d34ec9ccaa86ef048392c5f484734866a8cf751
Becky <you@example.com>
2019-06-24 10:28:56 -0400

    switched out dbs

---
+++ RestaurantRaterMVC/Models/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterMVC.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string FoodType { get; set; }
+        public double Rating { get; set; }
+
+    }
+
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
